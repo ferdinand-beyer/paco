763dd64d62a5efccf10c1a315dd792f101f05e38
Ferdinand Beyer <fb@fbeyer.com>
2024-01-08 09:50:57 +0100

    Set up CI

--- build.clj
+++ build.clj
@@ -4,23 +4,37 @@
 
 (def lib 'com.fbeyer/paco)
 (def version (format "0.1.%s" (b/git-count-revs nil)))
+
 (def class-dir "target/classes/clj")
 (def java-class-dir "target/classes/java")
-(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(def basis (delay (b/create-basis {:project "deps.edn"})))
 
 (defn clean [_]
   (b/delete {:path "target"}))
 
 (defn javac [_]
-  (b/javac {:basis basis
+  (b/javac {:basis @basis
             :src-dirs ["src"]
             :class-dir java-class-dir
             :javac-opts ["-Xlint:-options" "--release" "8"]}))
 
 (defn compile [_]
-  (b/compile-clj {:basis basis
+  (b/compile-clj {:basis @basis
                   :class-dir class-dir
                   :compile-opts {:direct-linking true}}))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis @basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources" java-class-dir]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
 
 (defn decompile [_]
   ;; https://vineflower.org/usage/
